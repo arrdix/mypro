e5ab77ad2a970bd94e026a27b202fbacc4a54c67
arrdix <mail.ardix@gmail.com>
2024-11-02 13:20:13 +0700

    feat(transaction): implement render transactions

--- src/components/transaction/transaction-card.tsx
+++ src/components/transaction/transaction-card.tsx
@@ -1,19 +1,27 @@
 import { Button } from '@/components/ui/button'
 import { FlatCard } from '@/components/ui/flat-card'
+import { Product } from '@/service/product/types/product.type'
 import { currencyFormatter } from '@/utils/helper'
 import { Copy, SquareArrowOutUpRight } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
-export function TransactionCard(): JSX.Element {
+interface TransactionCardProps {
+    code: string
+    products: Product[]
+    total: number
+}
+
+export function TransactionCard({ code, products, total }: TransactionCardProps): JSX.Element {
     function copyCode(): void {
         navigator.clipboard.writeText('TRX001')
     }
+
     return (
         <>
             <FlatCard>
                 <FlatCard.Header className="flex-row justify-between items-center">
                     <div className="flex items-center">
-                        <p className="font-bold pt-2 pb-1">TRX001</p>
+                        <p className="font-bold pt-2 pb-1">{code}</p>
                         <Button
                             variant="ghost"
                             className="text-muted hover:bg-transparent px-2"
@@ -23,7 +31,7 @@ export function TransactionCard(): JSX.Element {
                         </Button>
                     </div>
                     <Button variant="ghost" className="text-muted hover:bg-transparent pr-0">
-                        <Link to={`/transaction/TRX001`}>
+                        <Link to={`/transaction/${code}`}>
                             <SquareArrowOutUpRight />
                         </Link>
                     </Button>
@@ -32,14 +40,14 @@ export function TransactionCard(): JSX.Element {
                     <FlatCard.BodyItem>
                         <p className="text-xs text-muted">Products</p>
                         <p className="text-xl font-bold">
-                            20 <span className="text-xs">Pc(s)</span>
+                            {products.length} <span className="text-xs">Pc(s)</span>
                         </p>
                     </FlatCard.BodyItem>
                     <FlatCard.BodyItem>
                         <p className="text-xs text-muted">Total</p>
                         <div className="flex gap-1">
                             <p className="text-xs font-bold">RP</p>
-                            <p className="text-xl font-bold">{currencyFormatter(21000)}</p>
+                            <p className="text-xl font-bold">{currencyFormatter(total)}</p>
                         </div>
                     </FlatCard.BodyItem>
                 </FlatCard.Body>
--- src/components/transaction/transaction-overview.tsx
+++ src/components/transaction/transaction-overview.tsx
@@ -1,14 +1,26 @@
 import { Overview } from '@/components/ui/overview'
+import { Transaction } from '@/service/transaction/types/transaction.type'
 import { currencyFormatter } from '@/utils/helper'
+import { useMemo } from 'react'
 
-export function TransactionOverview(): JSX.Element {
+interface TransactionOverviewProps {
+    transactions: Transaction[]
+}
+
+export function TransactionOverview({ transactions }: TransactionOverviewProps): JSX.Element {
+    const revenue = useMemo(() => {
+        return transactions.reduce((currentRevenue, transaction) => {
+            return currentRevenue + transaction.Total
+        }, 0)
+    }, [transactions])
+
     return (
         <Overview>
             <Overview.Body>
                 <Overview.Title>Revenue</Overview.Title>
                 <Overview.Content>
                     <p className="text-sm font-bold">RP</p>
-                    <p className="text-4xl font-bold">{currencyFormatter(1200000)}</p>
+                    <p className="text-4xl font-bold">{currencyFormatter(revenue)}</p>
                 </Overview.Content>
             </Overview.Body>
             <Overview.ActionButton>Create Transaction</Overview.ActionButton>
